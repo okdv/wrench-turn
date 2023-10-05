e4a217badbb082a8995cf4167ad60e147c9cfbd1
okdv <okd@otho.dev>
2023-10-05 02:48:09 +0000

    fix: add text-link and text-blue, implement text-link

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,7 +3,9 @@ export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
     textColor: {
-      red: "#ef4444"
+      red: "#ef4444",
+      blue: "#3b82f6",
+      link: "#1d4ed8"
     },
     extend: {},
   },
