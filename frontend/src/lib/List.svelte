6b02efb895f3bf03cf999ddc1c2b9543a05e3110
okdv <okd@otho.dev>
2023-12-22 06:22:55 +0000

    feat: add dumb search to users page

---
+++ frontend/src/lib/List.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import type { User } from "$lib/types";
+
+    export let users: Array<User> = []
+</script>
+<div>
+    <ul>
+        {#each users as user}
+            <li>{user.username}</li>
+        {/each}
+    </ul>
+</div>
--- frontend/src/routes/users/+page.svelte
+++ frontend/src/routes/users/+page.svelte
@@ -1,8 +1,11 @@
 <script lang="ts">
 	import { getUsers } from "$lib/api";
 	import type { User } from "$lib/types";
+    import List from "$lib/List.svelte"
 
     let users: Array<User> = []
+    let filtered: Array<User> = []
+    let searchStr: string = ''
     let err: boolean = false
 
     const init = async() => {
@@ -15,17 +18,37 @@
         if (users.length === 0) {
             err = true
         }
+        filtered = users
     }
+
+    const updateSearch = async() => {
+        console.log(searchStr)
+        if (searchStr.length > 0) {
+            const matched = users.filter(user => {
+                if (user.username.toLowerCase().includes(searchStr.toLowerCase())) {
+                    return true 
+                } else if (user.description && user.description.toLowerCase().includes(searchStr.toLowerCase())) {
+                    return true 
+                }
+                return false 
+            })
+            console.log(matched)
+            filtered = matched
+        } else {
+            filtered = users
+        }
+    }
+
     init()
 </script>
 <div>
+    <div>
+        <input bind:value={searchStr} />
+        <button on:click={updateSearch}>Search</button>
+    </div>
     {#if err === true}
         <p>Looks a little empty, you should <a class="underline text-link" href="join"><b>join here</b></a></p>
     {:else}
-        <ul>
-            {#each users as user}
-                <li>{user.username}</li>
-            {/each}
-        </ul>
+        <List users={filtered} />
     {/if}
 </div>
